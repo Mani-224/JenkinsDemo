c44844d0f17e212f46f1318aacb7dbcdcd0e5e3b
ani-ruddha <aniruddhadas919@gmail.com>
2023-01-06 16:08:35 +0530

    Jenkins First msg

---
+++ JenkinsProject/src/main/java/Demo/DemoTest.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void m1()
+	{
+		Reporter.log("M1 Runnning");
+	}
+
+}
